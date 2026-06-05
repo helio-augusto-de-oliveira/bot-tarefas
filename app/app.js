b7bb43d7a4159ee81ffa3b115d11458c43e7b909
Hélio Augusto De Oliveira <gamespagos01@gmail.com>
2026-06-05 00:21:37 -0300

    Primeira versão funcional e limpa sem db

--- app/app.js
+++ app/app.js
@@ -1,105 +1,113 @@
-const axios = require('axios');
+const axios = require("axios");
 
-async function main() {
-    try {
-        // Login
-        const loginResponse = await axios.post(
-            'https://sedintegracoes.educacao.sp.gov.br/saladofuturobffapi/credenciais/api/LoginCompletoToken',
-            {
-                user: '00001109854377SP',
-                senha: 'Jl041108_'
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Ocp-Apim-Subscription-Key': 'd701a2043aa24d7ebb37e9adf60d043b'
-                }
+// =========================
+// CONFIGURAÇÃO
+// =========================
+
+const USER = "00001109854377SP";
+const SENHA = "Jl041108_";
+
+const SUBSCRIPTION_KEY =
+    "d701a2043aa24d7ebb37e9adf60d043b";
+
+// =========================
+// LOGIN SED
+// =========================
+
+async function fazerLogin() {
+    const { data } = await axios.post(
+        "https://sedintegracoes.educacao.sp.gov.br/saladofuturobffapi/credenciais/api/LoginCompletoToken",
+        {
+            user: USER,
+            senha: SENHA
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY
             }
-        );
-
-        const token = loginResponse.data.token;
-
-        console.log('Token:', token);
-
-        // Segunda requisição
-        const tokenResponse = await axios.post(
-            'https://edusp-api.ip.tv/registration/edusp/token',
-            {
-                token
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Origin': 'https://saladofuturo.educacao.sp.gov.br',
-                    'Referer': 'https://saladofuturo.educacao.sp.gov.br/',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0',
-                    'x-api-platform': 'webclient',
-                    'x-api-realm': 'edusp'
-                }
-            }
-        );
-
-        console.log("\n"+ tokenResponse.data.auth_token);
-
-   const itens = await axios.get(
-    "https://edusp-api.ip.tv/room/user?list_all=true&with_cards=true",
-    {
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Origin": "https://saladofuturo.educacao.sp.gov.br",
-            "Referer": "https://saladofuturo.educacao.sp.gov.br/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
-            "x-api-key": tokenResponse.data.auth_token
         }
-    }
-);
+    );
 
-const data = itens.data;
-const nickname = tokenResponse.data.nick;
-
-function gerarUrlTarefas(data, nickname) {
-    const params = new URLSearchParams({
-        expired_only: "false",
-        limit: "100",
-        offset: "0",
-        filter_expired: "true",
-        is_exam: "false",
-        with_answer: "true",
-        is_essay: "false",
-        answer_statuses: "draft",
-        with_apply_moment: "true"
-    });
-
-    for (const room of data.rooms || []) {
-        // publication_target=rxxxx
-        params.append("publication_target", room.name);
-
-        // publication_target=rxxxx:nickname
-        params.append(
-            "publication_target",
-            `${room.name}:${nickname}`
-        );
-
-        // publication_target=1931, 1698, etc
-        for (const category of room.group_categories || []) {
-            params.append(
-                "publication_target",
-                String(category.id)
-            );
-        }
-    }
-
-    return `https://edusp-api.ip.tv/tms/task/todo?${params.toString()}`;
+    return data.token;
 }
 
-function gerarUrlTarefasExpiradas(data, nickname) {
+// =========================
+// TOKEN CMSP
+// =========================
+
+async function obterAuth(token) {
+    const { data } = await axios.post(
+        "https://edusp-api.ip.tv/registration/edusp/token",
+        {
+            token
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Origin":
+                    "https://saladofuturo.educacao.sp.gov.br",
+                "Referer":
+                    "https://saladofuturo.educacao.sp.gov.br/",
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
+                "x-api-platform": "webclient",
+                "x-api-realm": "edusp"
+            }
+        }
+    );
+
+    return data;
+}
+
+// =========================
+// HEADERS PADRÃO
+// =========================
+
+function criarHeaders(authToken) {
+    return {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Origin":
+            "https://saladofuturo.educacao.sp.gov.br",
+        "Referer":
+            "https://saladofuturo.educacao.sp.gov.br/",
+        "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
+        "x-api-key": authToken
+    };
+}
+
+// =========================
+// SALAS
+// =========================
+
+async function obterSalas(authToken) {
+    const { data } = await axios.get(
+        "https://edusp-api.ip.tv/room/user?list_all=true&with_cards=true",
+        {
+            headers: criarHeaders(authToken)
+        }
+    );
+
+    return data;
+}
+
+// =========================
+// URL DE TAREFAS
+// =========================
+
+function gerarUrlTarefas(
+    salas,
+    nickname,
+    expiradas = false
+) {
     const params = new URLSearchParams({
-        expired_only: "true",
+        expired_only: String(expiradas),
         limit: "100",
         offset: "0",
-        filter_expired: "false",
+        filter_expired: String(!expiradas),
         is_exam: "false",
         with_answer: "true",
         is_essay: "false",
@@ -107,17 +115,20 @@ function gerarUrlTarefasExpiradas(data, nickname) {
         with_apply_moment: "true"
     });
 
-    for (const room of data.rooms || []) {
-        // rxxxx
-        params.append("publication_target", room.name);
+    for (const room of salas.rooms || []) {
+        // sala
+        params.append(
+            "publication_target",
+            room.name
+        );
 
-        // rxxxx:nickname
+        // sala:nickname
         params.append(
             "publication_target",
             `${room.name}:${nickname}`
         );
 
-        // ids das categorias
+        // categorias
         for (const category of room.group_categories || []) {
             params.append(
                 "publication_target",
@@ -129,53 +140,102 @@ function gerarUrlTarefasExpiradas(data, nickname) {
     return `https://edusp-api.ip.tv/tms/task/todo?${params.toString()}`;
 }
 
-// Exemplo:
-const url = gerarUrlTarefas(itens.data, nickname);
+// =========================
+// BUSCAR TAREFAS
+// =========================
 
-//console.log(url);
-    const getTarefasPendentes = await axios.get(
-        url,
-        {
-            headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Origin": "https://saladofuturo.educacao.sp.gov.br",
-            "Referer": "https://saladofuturo.educacao.sp.gov.br/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
-            "x-api-key": tokenResponse.data.auth_token
-            }
-        }
-    )
+async function obterTarefas(
+    salas,
+    nickname,
+    authToken,
+    expiradas = false
+) {
+    const url = gerarUrlTarefas(
+        salas,
+        nickname,
+        expiradas
+    );
 
-    console.log(getTarefasPendentes.data.length)
-    const urlExpiradas = gerarUrlTarefasExpiradas(itens.data, nickname);
-    const getTarefasExpiradas = await axios.get(
-        urlExpiradas,
-        {
-            headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Origin": "https://saladofuturo.educacao.sp.gov.br",
-            "Referer": "https://saladofuturo.educacao.sp.gov.br/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
-            "x-api-key": tokenResponse.data.auth_token
-            }
-        }
-    )
+    const { data } = await axios.get(url, {
+        headers: criarHeaders(authToken)
+    });
 
-    console.log(getTarefasExpiradas.data.length)
-
-    } catch (err) {
-        if (err.response) {
-            console.log('Status:', err.response.status);
-            console.log('Headers:', err.response.headers);
-            console.log('Body:', err.response.data);
-        } else {
-            console.error(err);
-        }
-    }
+    return data;
 }
 
+// =========================
+// ERROS
+// =========================
 
+function tratarErro(err) {
+    if (err.response) {
+        console.error("\n=== ERRO API ===");
+        console.error("Status:", err.response.status);
+        console.error("Body:", err.response.data);
+        return;
+    }
+
+    console.error(err);
+}
+
+// =========================
+// MAIN
+// =========================
+
+async function main() {
+    try {
+        console.log("Fazendo login...");
+
+        const token = await fazerLogin();
+
+        console.log("Obtendo auth_token...");
+
+        const auth = await obterAuth(token);
+
+        console.log("Obtendo salas...");
+
+        const salas = await obterSalas(
+            auth.auth_token
+        );
+
+        console.log(
+            `Salas encontradas: ${salas.rooms.length}`
+        );
+
+        console.log("Buscando tarefas pendentes...");
+
+        const tarefasPendentes =
+            await obterTarefas(
+                salas,
+                auth.nick,
+                auth.auth_token,
+                false
+            );
+
+        console.log("Buscando tarefas expiradas...");
+
+        const tarefasExpiradas =
+            await obterTarefas(
+                salas,
+                auth.nick,
+                auth.auth_token,
+                true
+            );
+
+        console.log("\n=== RESUMO ===");
+        console.log(
+            `Pendentes: ${tarefasPendentes.length}`
+        );
+        console.log(
+            `Expiradas: ${tarefasExpiradas.length}`
+        );
+
+        // Caso queira inspecionar:
+        // console.log(tarefasPendentes);
+        // console.log(tarefasExpiradas);
+    } catch (err) {
+        tratarErro(err);
+    }
+}
 
 main();
